a6d258066adc2e4d7013e3318566d75defa3541e
ELEF-TQ <tariq75aitlahklift@gmail.com>
2024-02-05 21:38:03 +0100

    setup project

---
+++ Server/src/test/java/com/Profolio/Gea/GeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Profolio.Gea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
